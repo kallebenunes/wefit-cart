133b9d9fcfe3561f618d290f2f3bfd6969958e4e
Kallebe Nunes <kallebenm@gmail.com>
2023-03-12 03:07:56 -0300

    style: config normalize

--- src/style/global.tsx
+++ src/style/global.tsx
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
+import normalize from 'styled-normalize'
 
 const GlobalStyle = createGlobalStyle`
+    ${normalize}
     html {
         background: ${props => props.theme.colors.primary};
         font-family: 'Open Sans', sans-serif;
