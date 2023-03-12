e286893626f086e6e90ec436d1578170f55d3914
Kallebe Nunes <kallebenm@gmail.com>
2023-03-12 02:14:54 -0300

    feat: config default theme colors

---
+++ src/style/themes/default/index.ts
@@ -0,0 +1,10 @@
+const theme = {
+    colors: {
+        primary: "#2F2E41",
+        white: "#FFF",
+        black: '#000000',
+        blue: '#009EDD',
+        green: '#039B00'
+    }
+}
+export default theme 
