2bda0f046a20134e1bd9496db00d5f2c801e5621
Kallebe Nunes <kallebenm@gmail.com>
2023-03-13 01:58:17 -0300

    feat: config routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,22 @@ import Header from "./components/Header"
 import Home from "./pages/home"
 import GlobalStyle from "./style/global"
 import theme from "./style/themes/default"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle/>
-      <Header/>
-      <OrderPlaced/>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle/>
+        <Header/>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/cart" Component={Cart}></Route>
+          <Route path="/orderPlaced" Component={OrderPlaced} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
